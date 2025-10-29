21807f05982adcdeb898e9406650788a418c405c
Lucas Conte <lucascontee@icloud.com>
2025-10-28 21:31:52 -0300

    initial api commit

---
+++ Calculadora-CO2.API/Controllers/TravelsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculadora_CO2.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TravelsController : ControllerBase
+    {
+        
+    
+    }
+}
---
+++ Calculadora-CO2.API/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Calculadora_CO2.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calculadora_CO2.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Travel> Travels { get; set; }
+    public DbSet<UserGoal> UserGoals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+        modelBuilder.Entity<Travel>()
+            .Property(t => t.Id)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Travel>()
+            .Property(t => t.CreatedAt)
+            .ValueGeneratedOnAdd()
+            .HasDefaultValueSql("GETDATE()");
+
+        modelBuilder.Entity<UserGoal>()
+            .Property(ug => ug.Id)
+            .ValueGeneratedOnAdd();
+    }
+
+
+}
