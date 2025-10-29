ff6e2fe052ba6c840118be55d8be2cbdf737497d
Lucas Conte <lucascontee@icloud.com>
2025-10-29 19:11:31 -0300

    Fix: clean cache and update gitignore

---
+++ Calculadora-CO2.API/Migrations/20251029003435_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calculadora_CO2.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Travels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    DistanceKm = table.Column<double>(type: "float", nullable: false),
+                    EmissionKg = table.Column<double>(type: "float", nullable: false),
+                    TransportType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TransportLabel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FuelType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Travels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGoals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MonthlyEmissionGoalKg = table.Column<double>(type: "float", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGoals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Travels");
+
+            migrationBuilder.DropTable(
+                name: "UserGoals");
+        }
+    }
+}
